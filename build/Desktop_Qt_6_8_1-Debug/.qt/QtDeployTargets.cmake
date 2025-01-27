949e10bf0320e0daafb0a783b365eddd6106b05a
Pactivisme <pavel@delhomme.ovh>
2025-01-27 20:33:33 +0100

    Supprimer les fichiers du dossier build du suivi Git

--- build/Desktop_Qt_6_8_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_simplebrowser_FILE /home/pactivisme/Qt/Examples/Qt-6.8.1/webenginewidgets/simplebrowser/build/Desktop_Qt_6_8_1-Debug/simplebrowser)
-set(__QT_DEPLOY_TARGET_simplebrowser_TYPE EXECUTABLE)
